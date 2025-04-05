d45435f20229e985ee81525a752d14709bc3ba9c
Zendhi Yuna Burnama <zendhiyb10@gmail.com>
2025-04-06 01:54:13 +0700

    + update code, card courses

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
 var nav = document.querySelector("nav");
 
 window.addEventListener("scroll", () => {
-    if (document.documentElement.scrollTop > 80) {
+    if (document.documentElement.scrollTop > 120) {
         nav.classList.add("sticky");
     } else {
         nav.classList.remove("sticky");
     }
 });
+
