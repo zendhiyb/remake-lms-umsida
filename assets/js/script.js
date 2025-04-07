c13a4df80c0cd89695533f9fd8b40ad9c96c1b93
Zendhi Yuna Burnama <zendhiyb10@gmail.com>
2025-04-08 01:13:12 +0700

    + update code, table

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,68 @@ window.addEventListener("scroll", () => {
     }
 });
 
+
+var options = {
+  series: [44, 55, 67, 83, 83, 83],
+  chart: {
+  height: 350,
+  type: 'radialBar',
+},
+plotOptions: {
+  radialBar: {
+    dataLabels: {
+      name: {
+        fontSize: '22px',
+      },
+      value: {
+        fontSize: '16px',
+      },
+      total: {
+        show: true,
+        label: 'Total',
+        formatter: function (w) {
+          // By default this function returns the average of all series. The below is just an example to show the use of custom formatter function
+          return 249
+        }
+      }
+    }
+  }
+},
+labels: ['Apples', 'Oranges', 'Bananas', 'Berries', 'Berries', 'Berries'],
+};
+
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+chart.render();
+
+
+  // var options = {
+  //   series: [{
+  //     name: 'Series 1',
+  //     data: [80, 50, 30, 40, 100, 20],
+  //   }],
+  //   chart: {
+  //     height: 700,
+  //     type: 'radar',
+  //   },
+  //   colors: ['#1C2A44'],
+  //   title: {
+  //     text: 'Basic Radar Chart'
+  //   },
+  //   yaxis: {
+  //     stepSize: 20
+  //   },
+  //   xaxis: {
+  //     categories: 
+  //     [
+  //       'GRAFIKA KOMPUTER', 
+  //       'KECERDASAN BUATAN', 
+  //       'PENGAMANAN SISTEM KOMPUTER', 
+  //       'MANAJEMEN PROYEK TEKNOLOGI INFORMASI', 
+  //       'DATA MINING', 
+  //       'PEMROGRAMAN MOBILE']
+  //   }
+  // };
+
+  // var chart3 = new ApexCharts(document.querySelector("#chart3"), options);
+  // chart3.render();
+  
