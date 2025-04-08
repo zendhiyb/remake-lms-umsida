2a4a6555bdb4add13f86fb2d959c312a2349a25d
Zendhi Yuna Burnama <zendhiyb10@gmail.com>
2025-04-09 02:22:13 +0700

    + update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,28 +8,6 @@ window.addEventListener("scroll", () => {
     }
 });
 
-
-// var options = {
-//   series: [70],
-//   chart: {
-//     height: 400,
-//     type: 'radialBar',
-//   },
-//   colors: ['#1C2A44'], // Warna radial bar
-//   plotOptions: {
-//     radialBar: {
-//       hollow: {
-//         size: '50%',
-//       }
-//     },
-//   },
-//   labels: ['Total'],
-// };
-
-// var chart = new ApexCharts(document.querySelector("#chart"), options);
-// chart.render();
-
-
 var options = {
   series: [44, 55, 67],
   chart: {
@@ -61,35 +39,3 @@ var options = {
 
 var chart = new ApexCharts(document.querySelector("#chart"), options);
 chart.render();
-
-  // var options = {
-  //   series: [{
-  //     name: 'Series 1',
-  //     data: [80, 50, 30, 40, 100, 20],
-  //   }],
-  //   chart: {
-  //     height: 700,
-  //     type: 'radar',
-  //   },
-  //   colors: ['#1C2A44'],
-  //   title: {
-  //     text: 'Basic Radar Chart'
-  //   },
-  //   yaxis: {
-  //     stepSize: 20
-  //   },
-  //   xaxis: {
-  //     categories: 
-  //     [
-  //       'GRAFIKA KOMPUTER', 
-  //       'KECERDASAN BUATAN', 
-  //       'PENGAMANAN SISTEM KOMPUTER', 
-  //       'MANAJEMEN PROYEK TEKNOLOGI INFORMASI', 
-  //       'DATA MINING', 
-  //       'PEMROGRAMAN MOBILE']
-  //   }
-  // };
-
-  // var chart3 = new ApexCharts(document.querySelector("#chart3"), options);
-  // chart3.render();
-  
